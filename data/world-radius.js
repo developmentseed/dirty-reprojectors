aaae7287a2aec7402d0a6b238103713a6e75d641
Derek Lieu <dl@dereklieu.com>
2016-11-12 15:51:04 -0600

    More projections

---
+++ data/world-radius.js
@@ -0,0 +1,1 @@
+module.exports = 6378137.0 // radius of Earth in meters
--- projections/world/airy.js
+++ projections/world/airy.js
@@ -1,3 +1,3 @@
 var d3 = require('d3-geo-projection')
 var R = require('../../data/world-radius')
-module.exports = d3.geoPeirceQuincuncial().scale(R).translate([0, 0])
+module.exports = d3.geoAiry().scale(R).radius(90).translate([0, 0])
